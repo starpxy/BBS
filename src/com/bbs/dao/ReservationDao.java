25d40b7b701b5682b833b9616fe0e4fc291092e1
starpxy <7301121@qq.com>
2017-05-08 15:08:42 +0800

    修改了Reservation的Dao

--- src/com/bbs/dao/ReservationDao.java
+++ src/com/bbs/dao/ReservationDao.java
@@ -1,5 +1,9 @@
 package com.bbs.dao;
 
+import com.bbs.entities.Reservation;
+
 public class ReservationDao extends BaseDao{
-	
+	public void createReservation(Reservation reservation){
+		getSession().saveOrUpdate(reservation);
+	}
 }
